d8caebcdc48fc63301ca20345960b0bb13b1260d
Denis Polenikov <polenikov17@gmail.com>
2022-10-12 21:16:30 +0300

    Added test's type

--- test_alarm.py
+++ test_alarm.py
@@ -10,6 +10,9 @@ class TestAlarm(unittest.TestCase):
         alarm.set_hour(10)
         self.assertEqual(alarm.get_hour(), 10)
 
+        alarm.set_type(1)
+        self.assertEqual(alarm.get_type(), 1)
+
 
     def test_values(self):
         alarm = Alarm(15, 15, 1)
@@ -17,6 +20,10 @@ class TestAlarm(unittest.TestCase):
         with self.assertRaises(ValueError):
             alarm.set_hour(25)
             alarm.set_hour(-40)
+
+            alarm.set_type(15)
+            alarm.set_type(0)
+            alarm.set_type(-2)
 
 
     def test_types(self):
@@ -26,6 +33,11 @@ class TestAlarm(unittest.TestCase):
             alarm.set_hour('1')
             alarm.set_hour([1, 2, 3])
             alarm.set_hour(True)
+
+            alarm.set_type('hello')
+            alarm.set_type(['a', 'b', 'c'])
+            alarm.set_type(False)
+
 
 
 
